93bfe38543013fbb5fa8054a09c64fd4685cbda0
Tanzkalmar35 <fabianholler2005@gmail.com>
2023-09-06 11:29:07 +0200

    added a rounded div following the cursor, z-index is not quite working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,3 @@
-
 function addAnimations() {
     let landingPageScrollTrigger = document.querySelector("#landingScrollTrigger");
 
@@ -6,5 +5,16 @@ function addAnimations() {
         console.log("scrolling");
         window.scrollTo(0, 0);
     });
-
 }
+
+window.onmousemove = e => {
+
+  const mouseTrailer = document.getElementById("cursorTrailer");
+
+  const x = e.clientX - mouseTrailer.offsetWidth,
+        y = e.clientY - mouseTrailer.offsetHeight;
+
+  mouseTrailer.style.transform = `translate(${x}px, ${y}px)`;  
+}
+
+
