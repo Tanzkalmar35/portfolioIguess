723bbc9f260abb09108052c8edc82d2bed476a9d
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2023-09-17 18:42:32 +0200

    little refactor

--- src/scripts/BaseAnimations.js
+++ src/scripts/BaseAnimations.js
@@ -11,7 +11,7 @@ window.onmousemove = e => {
   const x = e.clientX - mouseTrailer.offsetWidth / 2,
         y = e.clientY - mouseTrailer.offsetHeight / 2;
   const keyframes = {
-    transform: `translate(${x}px, ${y}px)`  
+    transform: `translate(${x}px, ${y}px)`
   }
   mouseTrailer.animate(keyframes, {
       duration: 800,
@@ -23,7 +23,6 @@ function enterMenuTrigger(event) {
   if (event.target === menuTrigger) {
     mouseTrailer.classList.add("w-[5rem]");
     mouseTrailer.classList.add("h-[5rem]");
-    mouseTrailer.classList.add("text-black");
   }
 }
 
