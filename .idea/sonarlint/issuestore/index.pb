b1f98756e7d5d3955bd417b11250b5adca772a23
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2023-09-30 23:38:17 +0200

    well, now we have another.

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,7 +3,17 @@ A
 package-lock.json,f\a\fa288d1472d29beccb489a676f68739ad365fc47
 <
 package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
-@
-public/style.css,c\1\c10e01865206f85032cf6199469b72569251c7f8
+?
+public/base.css,4\8\4808c9216e7cd456a14cfd4237d3db956fd774fb
+D
+src/scripts/utils.js,b\7\b723ccc2c51b9a3ddd056fdd76c7f5f3f68f4d42
+P
+ src/scripts/GalleryAnimations.js,8\9\891eb0b638a61e9562cb288d7b264c55d02651fd
 =
 src/style.css,1\9\199c81d8625cb1aaa85a72e49e4ad72a9fed6766
+@
+public/style.css,c\1\c10e01865206f85032cf6199469b72569251c7f8
+A
+public/index.html,f\4\f43ecdf8d666ee908aacce037b4cd72159e4741b
+M
+src/scripts/BaseAnimations.js,3\5\35c14284e38a0eff56b1826b4ca8b2884f10170b
--- src/scripts/BaseAnimations.js
+++ src/scripts/BaseAnimations.js
@@ -1,13 +1,23 @@
 const mouseTrailer = document.querySelector("#cursorTrailer")
+const loadingIndicator = document.querySelector("#enter-website")
+
+loadingIndicator.onclick = () => {
+    // Transition into the landing page
+    // Landing page is inspired by https://oxyco.webflow.io/
+}
 
 window.onmousemove = e => {
-  const x = e.clientX - mouseTrailer.offsetWidth / 2,
+    const x = e.clientX - mouseTrailer.offsetWidth / 2,
         y = e.clientY - mouseTrailer.offsetHeight / 2;
-  const keyframes = {
-    transform: `translate(${x}px, ${y}px)`
-  }
-  mouseTrailer.animate(keyframes, {
-      duration: 800,
-      fill: "forwards"
-  });
+    const keyframes = {
+        transform: `translate(${x}px, ${y}px)`
+    }
+    mouseTrailer.animate(keyframes, {
+        duration: 800,
+        fill: "forwards"
+    });
 }
+
+loadingIndicator.classList.add("loading_indicator_loaded");
+loadingIndicator.classList.add("cursor-pointer");
+loadingIndicator.innerHTML = "Enter";
