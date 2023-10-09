7e97456522bc7ca50d8969a105cb106511941922
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2023-10-09 22:38:16 +0200

    landing page kind of completed I guess.
    Now working on the about page.

--- src/animations.js
+++ src/animations.js
@@ -1,10 +1,11 @@
 //?  Gsap landing page animations
 
 const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
 
-// Defining animations
+const aboutPageTitleHeight = document.querySelector("#about-page-title").getBoundingClientRect().height;
 
-let slideOutLeft = gsap.timeline({
+let scrollAnimation = gsap.timeline({
     scrollTrigger: {
         scrub: 1,
         start: '0',
@@ -18,17 +19,36 @@ let animateTitleName = gsap.to("#landingpage-title-name", {
     duration: 1
 })
 
-let animateContactEmail = gsap.to(
-    "#landingpage-contact-email", {
-        x: -900,
-        duration: .7
-    }
-);
+let animateContactEmail = gsap.to("#landingpage-contact-email", {
+    x: -900,
+    duration: .7
+});
 
-// Starting all landing page animations simultaneously
+let animateTitleJobTitle = gsap.to("#landingpage-title-jobtitle", {
+    x: 1200,
+    duration: 1
+});
 
-slideOutLeft.add(animateTitleName, 0)
-slideOutLeft.add(animateContactEmail, 0)
+let animateContactLinks = gsap.to("#landingpage-contact-links", {
+    x: 1200,
+    duration: .7
+});
+
+let increaseLandingPageImageSize = gsap.to("#landingpage-profile", {
+    scale: 1.2,
+    duration: 1
+})
+
+let animateSecondSection = gsap.to("#about-page-container", {
+    y: -(screenHeight / 2) - (aboutPageTitleHeight / 2),
+})
+
+scrollAnimation.add(animateTitleName, 0)
+scrollAnimation.add(animateContactEmail, 0)
+scrollAnimation.add(animateContactLinks, 0)
+scrollAnimation.add(animateTitleJobTitle, 0)
+scrollAnimation.add(increaseLandingPageImageSize, 0)
+scrollAnimation.add(animateSecondSection, 1)
 
 // Lenis smooth scrolling
 
