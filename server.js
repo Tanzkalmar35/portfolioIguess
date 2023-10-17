7db6ef454190cada1193d68eb5dbcc0389ee179f
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2023-10-18 01:39:59 +0200

    completed routing, basic styling and file structure.

--- server.js
+++ server.js
@@ -6,11 +6,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Route all requests to the index.html for client-side routing
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/en/index.html');
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile(__dirname + '/public/about.html');
+    res.sendFile(__dirname + '/public/en/about.html');
+});
+
+app.get('/work', (req, res) => {
+    res.sendFile(__dirname + '/public/en/work.html');
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile(__dirname + '/public/en/404.html');
 });
 
 const port = process.env.PORT || 3000;
