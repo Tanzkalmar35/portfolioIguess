e3f6e2bc901c78fc2e9a4d897080a93d36132f34
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2023-10-18 14:22:41 +0200

    Added a horizontal scroll image slider for the work page.

---
+++ public/animations.js
@@ -0,0 +1,34 @@
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
+
+// Gsap animations
+let scrollAnimation = gsap.timeline({
+    scrollTrigger: {
+        scrub: 1,
+        start: '0',
+        end: screenWidth,
+        pin: document.querySelector("#workpage-content")
+    }
+})
+
+let horizontalScroll = gsap.to("#workpage-content", {
+    x: -screenWidth * 1.35,
+});
+
+let slideImages = gsap.to(".sliding-image", {
+    objectPosition: 0
+})
+
+scrollAnimation.add(horizontalScroll, 0);
+scrollAnimation.add(slideImages, 0);
+
+// Lenis smooth scrolling
+
+const lenis = new Lenis()
+
+function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
