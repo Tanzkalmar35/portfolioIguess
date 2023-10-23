762bfb5ceafe5554c10a8eaed0f5c7ebaf492c33
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2023-10-23 22:56:47 +0200

    I may don't have a brain gentlemen, but I have an idea...

--- public/animations.js
+++ public/animations.js
@@ -35,4 +35,29 @@ requestAnimationFrame(raf)
 
 // other user event animations
 
+// another image (the same) in full screen perfectly positioned with the transition from the exact position and size of the original to its desired location
 
+const images = document.querySelectorAll(".sliding-image");
+const fullscreenHandyImage = document.querySelector("#fullscreen-handy-image");
+
+images.forEach(function (image) {
+    image.addEventListener("click", () => {
+        const timeline = gsap.timeline();
+
+        // Define the animation steps for expanding the image
+
+        timeline.from(fullscreenHandyImage, {
+            width: image.getBoundingClientRect().width,
+            height: image.getBoundingClientRect().height,
+            left: image.getBoundingClientRect().left,
+            top: image.getBoundingClientRect().top,
+            duration: 2,
+            ease: "power3.inOut",
+        });
+
+        // Add a scroll event listener to reverse the animation
+        window.addEventListener("scroll", () => {
+            timeline.reverse();
+        });
+    });
+});
